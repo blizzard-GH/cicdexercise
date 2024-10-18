1ce9237fa8fb45df76fc36f334c59acbab351d39
Faisal Abdillah <fay.abdillah@gmail.com>
2024-10-18 12:37:20 +1100

    Deploying exercise.

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
     vue(),
     vueDevTools(),
   ],
-  base: "https://saram7619.github.io/deploying-vite-project-example/",
+  base: "https://blizzard-gh.github.io/cicdexercise/",
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
