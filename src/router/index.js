c0d0cb083e756e21d91bad4f61dbc5d998cd5fb7
Faisal Abdillah <fay.abdillah@gmail.com>
2024-10-18 20:52:11 +1100

    Trigger

--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,111 @@
-import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
-import AboutView from '../views/AboutView.vue'
+import { createRouter, createWebHistory } from 'vue-router';
+import store from '../store/store.js';
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: HomeView
+    component: () => import('../views/InformationPage.vue')
   },
   {
     path: '/about',
-    name: 'About',
-    component: AboutView
+    name: 'AboutUs',
+    component: () => import('../views/AboutUs.vue')
+  },
+  {
+    path: '/login',
+    name: 'LoginPage',
+    component: () => import('../views/LoginPage.vue')
+  },
+  {
+    path: '/userProfile',
+    name: 'UserProfilePage',
+    component: () => import('../views/UserProfile.vue')
+  },
+  {
+    path: '/contactUs',
+    name: 'ContactUs',
+    component: () => import('../views/ContactUs.vue')
+  },
+  {
+    path: '/donation',
+    name: 'DonationPage',
+    component: () => import('../views/DonationPage.vue')
+  },
+  {
+    path: '/userRegistration',
+    name: 'UserRegistrationPage',
+    component: () => import('../views/UserRegistration.vue')
+  },
+  {
+    path: '/newsFeed',
+    name: 'NewsFeed',
+    component: () => import('../views/NewsFeed.vue')
+  },
+  {
+    path: '/adminPage',
+    name: 'AdminPage',
+    component: () => import('../views/AdminPage.vue')
+  },
+  {
+    path: '/ratingPage',
+    name: 'RatingPage',
+    component: () => import('../views/RatingPage.vue')
+  },
+  {
+    path: '/fireLogin',
+    name: 'FireLogin',
+    component: () => import('../views/FirebaseLogin.vue')
+  },
+  {
+    path: '/fireRegister',
+    name: 'FireRegister',
+    component: () => import('../views/FirebaseRegistrationPage.vue')
+  },
+  {
+    path: '/addBook',
+    name: 'AddBook',
+    component: () => import('../views/AddBook.vue')
+  },
+  {
+    path: '/bookList',  // Note the change in the route name to correct capitalization
+    name: 'BookList',
+    component: () => import('../components/BookList.vue')
+  },
+  {
+    path: '/getBookCount',
+    name: 'GetBookCount',
+    component: () => import('../views/GetBookCountView.vue')
+  },
+  {
+    path: '/weatherCheck',
+    name: 'WeatherCheck',
+    component: () => import('../views/WeatherView.vue')
+  },
+  {
+    path: '/countBookAPI',
+    name: 'CountBookAPI',
+    component: () => import('../views/CountBookAPI.vue')
+  },
+  {
+    path: '/getAllBookAPI',
+    name: 'GetAllBookAPI',
+    component: () => import('../views/GetAllBookAPI.vue')
   }
-]
+];
 
 const router = createRouter({
   history: createWebHistory(process.env.BASE_URL),
   routes
-})
+});
 
-export default router
+router.beforeEach((to, from, next) => {
+  if (!store.state.isAuthenticated && to.name === 'UserProfile') {
+    alert('You must log in to view your profile.');
+    next({ name: 'LoginPage' });
+  } else {
+    next();
+  }
+});
+
+export default router;
