75a6b54bd2df7e4aa9744d6d635295743c77a289
Faisal Abdillah <fay.abdillah@gmail.com>
2024-10-18 12:50:08 +1100

    Trigger.

--- vite.config.js
+++ vite.config.js
@@ -6,14 +6,14 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
-  // plugins: [
-  //   vue(),
-  //   vueDevTools(),
-  // ],
-  base: "https://blizzard-gh.github.io/cicdexercise/"
-  // resolve: {
-  //   alias: {
-  //     '@': fileURLToPath(new URL('./src', import.meta.url))
-  //   }
-  // }
+  plugins: [
+    vue(),
+    vueDevTools(),
+  ],
+  base: "https://blizzard-gh.github.io/cicdexercise/",
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
